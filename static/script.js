a423aad1438f0d8cf5593e133904ad7e32e30228
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2023-09-19 15:07:36 +0200

    updated to fix read article list

--- static/script.js
+++ static/script.js
@@ -1,57 +1,91 @@
-// Reroll button
+// Initialize user statistics
+let totalArticles = 0;
+let currentStreak = 0;
+let readArticles = [];
+
+// Function to update user statistics on the page
+function updateUserStats() {
+    document.getElementById('total-articles').textContent = totalArticles;
+    document.getElementById('current-streak').textContent = currentStreak;
+}
+
+// Function to update the list of read articles
+function updateReadArticles() {
+  const articleList = document.getElementById('article-list');
+  articleList.innerHTML = '';
+  readArticles.forEach(article => {
+      const listItem = document.createElement('li');
+      const anchor = document.createElement('a');
+      anchor.href = article.url;
+      anchor.textContent = article.title;
+      anchor.target = '_blank';  // Opens the link in a new tab
+      listItem.appendChild(anchor);
+      articleList.appendChild(listItem);
+  });
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  const rerollButton = document.getElementById('reroll-button');
-  
-  rerollButton.addEventListener('click', function() {
-    // Fetch new articles from the server
-    fetch('/new_articles')
-      .then(response => response.json())
-      .then(newArticles => {
-        // Find all the existing cards
-        const cardElements = document.querySelectorAll('.card');
-        
-        // Update each card with new article data
-        cardElements.forEach((cardElement, index) => {
-          const newArticle = newArticles[index];
-          cardElement.parentElement.href = newArticle.url;
-          cardElement.querySelector('.card-title').textContent = newArticle.title;
-          cardElement.querySelector('.card-excerpt').textContent = newArticle.first_sentence;
-          cardElement.querySelector('.card-views').textContent = `views last month: ${newArticle.views}`;  // Update views
-          if (newArticle.img_url) {
-              cardElement.querySelector('.card-thumb img').src = newArticle.img_url;
-          }
-      });
+    const rerollButton = document.getElementById('reroll-button');
+    const toggleReadArticlesButton = document.getElementById('toggle-read-articles');
+    const readArticlesList = document.getElementById('read-articles-list');
 
-
-      })
-      .catch(error => {
-        console.error('Error fetching new articles:', error);
-      });
-  });
-});
-
-// Save to JSON
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("Card clicked!"); // for testing
-  const cardElements = document.querySelectorAll(".card");
-
-  cardElements.forEach(card => {
-    card.addEventListener("click", function() {
-      const title = card.querySelector(".card-title").textContent;
-      const firstSentence = card.querySelector(".card-excerpt").textContent;
-      const url = card.closest('a').getAttribute('href');
-
-      fetch("/handle_click", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: title,
-          firstSentence: firstSentence,
-          url: url,
-        }),
-      });
+    // Toggle read articles list
+    toggleReadArticlesButton.addEventListener('click', function() {
+        readArticlesList.classList.toggle('hidden');
     });
-  });
+
+    rerollButton.addEventListener('click', function() {
+        fetch('/new_articles')
+            .then(response => response.json())
+            .then(newArticles => {
+                const cardElements = document.querySelectorAll('.card');
+                cardElements.forEach((cardElement, index) => {
+                    const newArticle = newArticles[index];
+                    cardElement.parentElement.href = newArticle.url;
+                    cardElement.querySelector('.card-title').textContent = newArticle.title;
+                    cardElement.querySelector('.card-excerpt').textContent = newArticle.first_sentence;
+                    cardElement.querySelector('.card-views').textContent = `views last month: ${newArticle.views}`;
+                    if (newArticle.img_url) {
+                        cardElement.querySelector('.card-thumb img').src = newArticle.img_url;
+                    }
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching new articles:', error);
+            });
+    });
+
+    const cardElements = document.querySelectorAll(".card");
+    cardElements.forEach(card => {
+        card.addEventListener("click", function() {
+            const title = card.querySelector(".card-title").textContent;
+            const firstSentence = card.querySelector(".card-excerpt").textContent;
+            const url = card.closest('a').getAttribute('href');
+
+            fetch("/handle_click", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    title: title,
+                    firstSentence: firstSentence,
+                    url: url,
+                }),
+            });
+
+            // Increment total articles read
+            totalArticles++;
+            // Increment current streak (this is a placeholder; you'll likely want more complex logic)
+            currentStreak++;
+
+            // Add to read articles
+            readArticles.push({ title: title, firstSentence: firstSentence, url: url });
+
+            // Update user statistics and read articles
+            updateUserStats();
+            updateReadArticles();
+        });
+    });
 });
